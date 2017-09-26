3589849c262012e8821ecb461275c9d2d1fade41
Chris Rae <clr@chrisrae.com>
2017-09-26 09:46:02 -0700

    Time format now successfully removing seconds

--- Farm/Program.cs
+++ Farm/Program.cs
@@ -18,7 +18,7 @@ namespace Farm
         }
         static string ToReadableString(TimeSpan span)
         {
-            string formatted = string.Format("{0}{1}{2}{3}",
+            string formatted = string.Format("{0}{1}{2}",
                 span.Duration().Days > 0 ? string.Format("{0:0} day{1}, ", span.Days, span.Days == 1 ? String.Empty : "s") : string.Empty,
                 span.Duration().Hours > 0 ? string.Format("{0:0} hour{1}, ", span.Hours, span.Hours == 1 ? String.Empty : "s") : string.Empty,
                 span.Duration().Minutes > 0 ? string.Format("{0:0} minute{1}, ", span.Minutes, span.Minutes == 1 ? String.Empty : "s") : string.Empty);

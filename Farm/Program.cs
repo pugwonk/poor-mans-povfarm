7ab960aadaf3be76ed80c86db184580bcc22d099
Chris Rae <clr@chrisrae.com>
2017-09-26 09:39:15 -0700

    Calculating ETA

--- Farm/Program.cs
+++ Farm/Program.cs
@@ -11,6 +11,25 @@ namespace Farm
 {
     class Program
     {
+        // https://stackoverflow.com/questions/842057/how-do-i-convert-a-timespan-to-a-formatted-string
+        static string ToReadableAgeString(TimeSpan span)
+        {
+            return string.Format("{0:0}", span.Days / 365.25);
+        }
+        static string ToReadableString(TimeSpan span)
+        {
+            string formatted = string.Format("{0}{1}{2}{3}",
+                span.Duration().Days > 0 ? string.Format("{0:0} day{1}, ", span.Days, span.Days == 1 ? String.Empty : "s") : string.Empty,
+                span.Duration().Hours > 0 ? string.Format("{0:0} hour{1}, ", span.Hours, span.Hours == 1 ? String.Empty : "s") : string.Empty,
+                span.Duration().Minutes > 0 ? string.Format("{0:0} minute{1}, ", span.Minutes, span.Minutes == 1 ? String.Empty : "s") : string.Empty);
+
+            if (formatted.EndsWith(", ")) formatted = formatted.Substring(0, formatted.Length - 2);
+
+            if (string.IsNullOrEmpty(formatted)) formatted = "0 seconds";
+
+            return formatted;
+        }
+
         static void Main(string[] args)
         {
             // Check every so often for new files
@@ -46,14 +65,14 @@ namespace Farm
                         if (line.StartsWith("Initial_Frame="))
                             firstFrame = int.Parse(line.Substring("Initial_Frame=".Length));
                     }
-                    Console.WriteLine("First frame is " + firstFrame.ToString());
-                    Console.WriteLine("Last frame is " + lastFrame.ToString());
+                    //Console.WriteLine("First frame is " + firstFrame.ToString());
+                    //Console.WriteLine("Last frame is " + lastFrame.ToString());
                     if (lastFrame == 0)
                     {
                         Console.WriteLine("No animation in INI.");
                         return true;
                     }
-                    Console.WriteLine("Picking a random frame...");
+                    //Console.WriteLine("Picking a random frame...");
                     int[] frames = new int[lastFrame - firstFrame + 1];
                     for (int i = firstFrame; i <= lastFrame; i++)
                     {
@@ -64,14 +83,29 @@ namespace Farm
                     int padding = lastFrame.ToString().Length;
                     var newName = Path.GetFileNameWithoutExtension(iniFile.FullName);
                     int doingFrame = 0;
+                    int doneFrames = 0;
+                    DateTime firstFrameAt = DateTime.Now;
+                    DateTime lastFrameAt = new DateTime(0);
                     foreach (int fn in randomFrames)
                     {
                         string imgFile = newName + fn.ToString(new string('0', padding)) + ".png";
-                        if (!File.Exists(imgFile))
+                        if (File.Exists(imgFile))
                         {
-                            Console.WriteLine("Found undrawn frame at " + imgFile);
-                            doingFrame = fn;
-                            break;
+                            // Store times for ETA
+                            doneFrames++;
+                            DateTime thisOne = File.GetLastWriteTime(imgFile);
+                            if (thisOne < firstFrameAt)
+                                firstFrameAt = thisOne;
+                            if (thisOne > lastFrameAt)
+                                lastFrameAt = thisOne;
+                        }
+                        else
+                        {
+                            if (doingFrame == 0) // haven't already picked one
+                            {
+                                Console.WriteLine("Found undrawn frame at " + imgFile);
+                                doingFrame = fn;
+                            }
                         }
                     }
                     if (doingFrame == 0)
@@ -81,6 +115,18 @@ namespace Farm
                     }
                     else
                     {
+                        // Calculate EA
+                        if (doneFrames > 1)
+                        {
+                            TimeSpan tookSoFar = lastFrameAt - firstFrameAt;
+                            int totFrames = lastFrame - firstFrame;
+                            // Because this only takes creation time, it doesn't account for the time the
+                            // first frame took to draw, hence the doneFrames -1. Although who knows if that's
+                            // the right way to do this
+                            TimeSpan timeLeft = TimeSpan.FromTicks((long)(tookSoFar.Ticks * ((double)(doneFrames - 1) / totFrames)));
+                            DateTime eta = DateTime.Now + timeLeft;
+                            Console.WriteLine("Have done " + doneFrames.ToString() + "/" + totFrames.ToString() + " in " + ToReadableString(tookSoFar) + ". ETA is " + eta.ToString());
+                        }
                         if (lockWorks(doingFrame))
                         {
                             // Go ahead and render
